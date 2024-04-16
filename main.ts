4fd8b99fbcd42bf8345d6afd1927eed9a8b8f5a3
Geert Eltink <geert.eltink@gmail.com>
2024-04-16 23:17:27 +0200

    feat: sync feeds

--- main.ts
+++ main.ts
@@ -1,134 +1,118 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin, TFile } from "obsidian";
+import Parser from "rss-parser";
+import { format } from "date-fns/format";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class ReadLaterPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		await this.updateFeeds();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// This function will automatically clear the interval when the plugin is disabled.
+		this.registerInterval(
+			window.setInterval(() => this.updateFeeds(), 60 * 60 * 1000)
+		);
+	}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	onunload() {}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	async updateFeeds() {
+		const folder = this.app.vault.getFolderByPath("read later");
+		if (folder === null) {
+			console.error("Read Later - Folder does not exist");
+			return;
+		}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		console.log("Read Later - Syncing feeds");
+
+		for (const category of folder.children) {
+			if (category instanceof TFile) {
+				const lastSynced = this.extractSyncedTime(category);
+				const feeds = this.extractFeeds(category);
+				if (feeds === null) {
+					return;
+				}
+
+				for (const feed of feeds) {
+					await this.updateFeed(category, feed, lastSynced);
+					await this.updateSyncedTime(category);
 				}
 			}
+		}
+	}
+
+	extractSyncedTime(category: TFile): Date {
+		const metadataCache = this.app.metadataCache.getFileCache(category);
+		const frontMatterCache = metadataCache?.frontmatter;
+
+		const defaultDate = new Date();
+		defaultDate.setFullYear(new Date().getFullYear() - 3);
+
+		if (frontMatterCache === undefined || !frontMatterCache.xml_synced) {
+			return defaultDate;
+		}
+
+		return new Date(frontMatterCache.xml_synced);
+	}
+
+	extractFeeds(category: TFile): string[] | null {
+		const metadataCache = this.app.metadataCache.getFileCache(category);
+		const frontMatterCache = metadataCache?.frontmatter;
+
+		if (frontMatterCache === undefined) {
+			return null;
+		}
+
+		return frontMatterCache.xml_feeds;
+	}
+
+	async updateFeed(file: TFile, url: string, lastSynced: Date) {
+		let content = await this.app.vault.read(file);
+		const parser = new Parser({
+			defaultRSS: 2.0,
+			timeout: 10000,
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		try {
+			const feed = await parser.parseURL(url);
+			for (const entry of feed.items) {
+				if (!entry.isoDate || lastSynced < new Date(entry.isoDate)) {
+					content.trimEnd();
+					content += `- [ ] [${entry.title}](${entry.link}) _(${
+						feed.title ?? feed.link
+					})_\n`;
+				}
+			}
+		} catch (error) {
+			return;
+		}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		await this.app.vault.modify(file, content);
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+	async updateSyncedTime(file: TFile) {
+		try {
+			await this.app.fileManager.processFrontMatter(
+				file,
+				(frontmatter) => {
+					frontmatter["xml_synced"] = formatTimestamp(
+						file.stat.mtime
+					);
+				}
+			);
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (e: any) {
+			if (e?.name === "YAMLParseError") {
+				const errorMessage = `Timestamp failed to update because of malformed frontmatter on this file : ${file.path} ${e.message}`;
+				new Notice(errorMessage, 4000);
+				console.error(errorMessage);
+				return {
+					status: "error",
+					error: e,
+				};
+			}
+		}
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+function formatTimestamp(input: number): string {
+	return format(new Date(input), "yyyy-MM-dd'T'HH:mm");
 }
